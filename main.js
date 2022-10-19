ef9a80933e55b7dbaf884e532f13f07f00a16e8f
Aldiyar <aimamadiev@mail.ru>
2022-10-19 13:28:51 +0700

    add object

--- main.js
+++ main.js
@@ -1,28 +1,8 @@
+let user = {
+	name: "John",
+	age: 30
+};
 
-function calc(from, a, b) {
-	let operations = {
-		add: '+',
-		multy: '*',
-		subtract: '-',
-	}
-	switch (from) {
-		case operations.add:
-			result = a + b;
-			alert('result of + is ' + result);
-		case operations.multy:
-			result = a * b;
-			alert('result of * is ' + result);
-		case operations.subtract:
-			result = a - b;
-			alert('result of - is ' + result);
+let clone = Object.assign({}, user);
 
-	}
-}
-
-
-let a = prompt("Введите значение a")
-let b = prompt("Введите значение b")
-let mathValue = prompt("Введите математическую операцию  :")
-
-calc(mathValue, a, b);
-
+console.log(clone)
