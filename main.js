192f57ef77bb3211e433b09905648bbbd1024304
Aldiyar <aimamadiev@mail.ru>
2022-11-09 19:52:33 +0700

    6 kyu задача с лицами

--- main.js
+++ main.js
@@ -1,56 +1,12 @@
-/* function abbrevName(name) {
-	let arr = name.split(" ")
-	// code away
-
-	//console.log(arr[0].slice(0, 1) + '.' + arr[1].slice(0, 1))
-	console.log(arr[0].toUpperCase().slice(0, 1) + '.' + arr[1].toUpperCase().slice(0, 1))
-}
-
-
-
-
-abbrevName("David mendieta")
-abbrevName("Sam Harris")
-abbrevName("OGWXX cDiZTeMOHc")
-abbrevName("patrick feeney")
-abbrevName("Sam Harris") */
-
-function XO(str) {
-	let stroka = str.split('')
-	let countX = 0;
-	let countO = 0;
-	let nothing = 0;
-	for (let part of stroka) {
-
-		if ((part == "x") || (part == "X")) {
-			countX += 1;
-		} else if ((part == "o") || (part == "O")) {
-			countO += 1;
-		} else {
-			nothing += 1;
+function countSmileys(arr) {
+	let count = 0;
+	for (let face of arr) {
+		if ((face[0] == ';' || face[0] == ':') && ((face[1] == 'D' || face[1] == ")") || (face[2] == 'D' || face[2] == ')'))) {
+			count = count + 1;
 		}
 	}
+	console.log(count)
 
-	if (countX == countO) {
-		return (true)
-	} else {
-		return (false)
-	}
 }
 
-
-console.log(XO('oox'))
-console.log(XO('gdfkgdfgdfgjk'))
-console.log(XO('ooom'))
-console.log(XO('oxx'))
-console.log(XO('oofdfxx'))
-console.log(XO('oOoxX'))
-
-
-
-
-
-
-
-
-
+countSmileys([':D', ':~)', ';~D', ':-)', ';]'])
