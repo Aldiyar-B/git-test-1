d944c07b81296aa467e261cc8b6ecd66554f0ae3
Aldiyar <aimamadiev@mail.ru>
2022-11-12 22:27:25 +0700

    addad

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-function countSmileys(arr) {
-	let count = 0;
-	for (let face of arr) {
-		if ((face[0] == ';' || face[0] == ':') && ((face[1] == 'D' || face[1] == ")") || (face[2] == 'D' || face[2] == ')'))) {
-			count = count + 1;
-		}
-	}
-	console.log(count)
+сonst elemOne = document.querySelector('[data-say-hi]');
 
-}
-
-countSmileys([':D', ':~)', ';~D', ':-)', ';]'])
+console.log(elemOne.dataset.sayHi);
