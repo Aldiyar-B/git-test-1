20e9cb49a990d96560e85e73708a40516e0ce43d
Aldiyar <aimamadiev@mail.ru>
2022-11-12 22:10:32 +0700

    add calc #40

--- main.js
+++ main.js
@@ -1,12 +1,38 @@
-function countSmileys(arr) {
-	let count = 0;
-	for (let face of arr) {
-		if ((face[0] == ';' || face[0] == ':') && ((face[1] == 'D' || face[1] == ")") || (face[2] == 'D' || face[2] == ')'))) {
-			count = count + 1;
-		}
+const result = document.getElementById('result');
+button.addEventListener("click", calc);
+document.querySelector('.history').addEventListener("click", deleteHistory);
+
+
+function calc() {
+	const num1 = document.getElementById('firstNumber').value;
+	const num2 = document.getElementById('secondNumber').value;
+	const action = document.getElementById('mathActions').value;
+	switch (action) {
+		case '+':
+			result.textContent = +num1 + +num2;
+			break;
+		case '-':
+			result.textContent = num1 - num2;
+			break;
+		case '*':
+			result.textContent = num1 * num2;
+			break;
+		case '/':
+			result.textContent = num1 / num2;
+			break;
 	}
-	console.log(count)
+
+	let div = document.createElement('div');
+	div.innerHTML = result.textContent; //textContent
+	document.querySelector('.history').append(div);
+
 
 }
 
-countSmileys([':D', ':~)', ';~D', ':-)', ';]'])
+function deleteHistory() {
+	document.querySelector('.history').addEventListener('click', (e) => {
+		const targetResult = e.target;
+		targetResult.remove();
+	});
+
+} 
