c4753fa942e5c50ac503f184785ca74db2fb2383
Aldiyar <aimamadiev@mail.ru>
2022-11-24 21:46:01 +0700

    male or female

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-сonst elemOne = document.querySelector('[data-say-hi]');
 
-console.log(elemOne.dataset.sayHi);
+const serverUrl = 'https://api.genderize.io';
+
+let input = document.querySelector('.content');
+input.addEventListener('submit', (event) => {
+	event.preventDefault();
+	f(document.querySelector('.input').value)
+})
+
+async function f(userName) {
+	const url = `${serverUrl}?name=${userName}`;
+	const response = await fetch(url);
+	const data = await response.json();
+	const gender = data.gender;
+
+	console.log(`${userName} is ${gender}`)
+
+}
