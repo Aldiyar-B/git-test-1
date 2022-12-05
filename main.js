5f59f9e3baddd69c8fd189a5594757c390e0da06
Aldiyar <aimamadiev@mail.ru>
2022-12-05 08:16:12 +0700

    add https

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const SERVER = {
-	URL: 'http://api.openweathermap.org/data/2.5/weather',
+	URL: 'https://api.openweathermap.org/data/2.5/weather',
 	API_KEY: 'f660a2fb1e4bad108d6160b7f58c555f'
 }
 
