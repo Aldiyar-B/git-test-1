27f7dbccefb8d26e8f911d11e0506b2e4aae023a
Aldiyar <aimamadiev@mail.ru>
2023-02-01 13:06:51 +0700

    Aldi/practice-22 Прототипы

--- main.js
+++ main.js
@@ -1,16 +1,84 @@
-const person = {
-	nationality: 'Kazakh',
-	citizen: "Russia",
-	height: 173,
-	weight: 60,
-	name: 'Aldiyar',
-	hobby: 'Football',
-	friends: {
-		best: "Mirlan",
-		brother: "Alisher",
-		highBro: "Temirlan",
-
+/* const person = new Object({
+	name: 'Mikhail',
+	age: 18,
+	hobby: 'sport',
+	hi: function () {
+		console.log('hiii!')
 	}
+})
+
+Object.prototype.sayGoodBye = function () {
+	console.log('Good bye!')
 }
 
-console.log(`his name is ${person.name} and he has some friends: ${person.friends?.best},${person.friends?.brother},${person.friends?.highBro}.`)
+Object.prototype.sayGoodBye = function () {
+	console.log('poka')
+}
+
+const Aldi = Object.create(person)
+
+Aldi.name = 'Aldik';
+
+const dog = 'I am a dog' */
+
+
+//Значение this
+/* const animal = {
+	walk() {
+		if (!this.isSleeping) {
+			console.log('I walk')
+		}
+	},
+
+	sleep() {
+		this.isSleeping = true;
+		console.log('I sleep')
+	}
+};
+
+let rabbit = {
+	name: "Bags Bonny",
+	__proto__: animal,
+
+}
+
+let penguin = {
+	name: 'Rico',
+	__proto__: animal,
+
+} */
+
+// Цикл For..in
+
+
+const animal = {
+	walk() {
+		if (!this.isSleeping) {
+			console.log('I walk')
+		}
+	},
+
+	sleep() {
+		this.isSleeping = true;
+		console.log('I sleep')
+	}
+};
+
+let rabbit = {
+	name: "Bags Bonny",
+	__proto__: animal,
+
+}
+
+let penguin = {
+	name: 'Rico',
+	__proto__: animal,
+
+}
+
+console.log(Object.keys(penguin))
+
+for (let key in penguin) {
+
+	console.log(key);
+}
