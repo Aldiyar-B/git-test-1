43179f45a83586fa3c0f59b8be3f43680c1be6e2
Aldiyar <aimamadiev@mail.ru>
2023-02-02 21:50:31 +0700

    add call,apply,bind

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
-class User {
-	constructor(first, last) {
-		this.first = first;
-		this.last = last;
-
-	}
-
-	set first(value) {
-		this.firstName = value.toUpperCase();
-	}
-	set last(value) {
-		this.lastName = value.toUpperCase();
-	}
-	get fullName() {
-		return this.firstName + " " + this.lastName;
-	}
-};
+/* function sayHi() {
+	console.log(this.name)
+}
 
 
+let user = {
+	name: 'Hola'
+}
+let user2 = {
+	name: 'Figo'
+}
 
-let user1 = new User('Vlad', 'Kapustin')
-let user2 = new User('Ivan', 'Semenov')
+sayHi.call(user)
+sayHi.call(user2) */
 
-console.log(user1.fullName)
-console.log(user2.fullName)
+
+// ---------------
+
+function sayHi(age, sex) {
+	console.log(`Hi ${this.name}, your age is ${age} and you is  ${sex}`)
+}
+
+let userInfo = {
+	name: 'John'
+}
+
+sayHi.call(userInfo, 14, 'male')
