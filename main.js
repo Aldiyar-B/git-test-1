e56a375627b35959edcf2d2db4091b5aa6b2a310
Aldiyar <aimamadiev@mail.ru>
2023-02-09 12:55:38 +0700

    XMLHttpRequest

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
-/* function sayHi() {
-	console.log(this.name)
+/* 
+// 1. Создаём новый XMLHttpRequest-объект
+let xhr = new XMLHttpRequest;
+
+// 2. Настраиваем его: GET-запрос по URL /article/.../load
+xhr.open('GET', '/article/xmlhttprequest/example/load');
+
+// 3. Отсылаем запрос
+xhr.send();
+
+// 4. Этот код сработает после того, как мы получим ответ сервера
+xhr.onload = function () {
+	if (xhr.status != 200) {
+		alert(`Ошибка ${xhr.status}:  ${xhr.statusText}`)
+	} else {
+		alert(`Готово, получили ${xhr.response.length} байт `)
+	}
+};
+
+xhr.onprogress = function (event) {
+	if (event.lengthComputable) {
+		alert(`Получено ${event.loaded} из ${event.total} байт`)
+	} else {
+		alert(`Получено ${event.loaded} байт`);
+	}
+};
+
+xhr.onerror = function () {
+	alert(`Запрос не удался`)
 }
-
-
-let user = {
-	name: 'Hola'
-}
-let user2 = {
-	name: 'Figo'
-}
-
-sayHi.call(user)
-sayHi.call(user2) */
-
-
-// ---------------
-
-function sayHi(age, sex) {
-	console.log(`Hi ${this.name}, your age is ${age} and you is  ${sex}`)
-}
-
-let userInfo = {
-	name: 'John'
-}
-
-sayHi.call(userInfo, 14, 'male')
+ */
